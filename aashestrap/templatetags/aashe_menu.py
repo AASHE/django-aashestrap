32d792da76523c0dc65b3e91d71e24324974185b
Scott Johnson <baronvonvaderham@gmail.com>
2016-08-11 12:00:29 -0400

    Fix Templatetag Queries
    Wrap objects.get() calls within templatetags in try/except to fix broken
    tests on travis for SCPD and Bulletin. Was not creating the Menu objects
    inside tests, so loading those tags with get(pk=1) was returning
    errors becuase that object was never created, causing the whole page request
    to fail.

--- aashestrap/templatetags/aashe_menu.py
+++ aashestrap/templatetags/aashe_menu.py
@@ -3,16 +3,23 @@
 """
 from django import template
 from aashestrap.models import Menu
+from django.core.exceptions import ObjectDoesNotExist
 
 register = template.Library()
 
 
 @register.simple_tag
 def drupal_footer_menu():
-    menu = Menu.objects.get(pk=1)
-    return menu.footer
+    try:
+        menu = Menu.objects.get(pk=1)
+        return menu.footer
+    except ObjectDoesNotExist:
+        return None
 
 @register.simple_tag
 def drupal_header_menu():
-    menu = Menu.objects.get(pk=1)
-    return menu.header
+    try:
+        menu = Menu.objects.get(pk=1)
+        return menu.header
+    except ObjectDoesNotExist:
+        return None
